8e9dcb057aa982db3087e063d45ac7b4cdfa796c
Ubuntu <ubuntu@ip-172-31-4-71.us-east-2.compute.internal>
2021-02-23 02:59:36 +0000

    committing 2 new files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import java.io.*
+class HelloWorld 
+{
+	public static void main String(args [])
+	{
+Systemout.println("Welcome to java");
+	}
+}
---
+++ NameIp.java
@@ -0,0 +1,8 @@
+import java.io.*
+class NameIP
+{
+	public static void main String(args [])
+	{
+Systemout.println("Welcome to java");
+	}
+}
